b86848171e8134e12e124d77265c0b5f0b6b9af8
Eshahani1 <112349703+Eshahani1@users.noreply.github.com>
2022-09-26 14:29:42 -0400

    Update defaulscripts.js

--- itis3135/scripts/defaulscripts.js
+++ itis3135/scripts/defaulscripts.js
@@ -5,5 +5,4 @@ function scriptTest(){
 function welcoming_user(){
     document.getElementById("question").submit();
     document.getElementById("question").innerHTML = "Welcome" + first_name + ", we are glad you are feeling " + mood + " today";
-    
 }
