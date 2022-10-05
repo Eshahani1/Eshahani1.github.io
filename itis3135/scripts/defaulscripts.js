7348a1a980ec16e151d52f0c33f3a792321dfe99
Eshahani1 <112349703+Eshahani1@users.noreply.github.com>
2022-10-05 14:20:30 -0400

    Update defaulscripts.js

--- itis3135/scripts/defaulscripts.js
+++ itis3135/scripts/defaulscripts.js
@@ -1,24 +1,24 @@
-function scriptTest(){
+function scriptTest() {
     alert("Hey my script is running");
-}
-
-function welcoming_user(){
+  }
+  
+  function welcoming_user() {
     let first = document.getElementById('first_name').value;
     let mood = document.getElementById('mood').value;
     document.getElementById("answers").innerHTML = ("The ES welcomes you, " + first + "." + " we're glad you are doing " + mood + "!");
-}
-
-function display_date(){
+  }
+  
+  function display_date() {
     let today = new Date().toLocaleDateString();
     let today2 = new Date().toLocaleTimeString();
     document.getElementById("current_date").innerHTML = ("The current Date is: " + today + " The local time is: " + today2);
-}
-
-function simpleAddition(){
+  }
+  
+  function simpleAddition() {
     let first_number = document.getElementById("first_number").value;
     let second_number = document.getElementById("second_number").value;
     let answer = (Number(first_number) + Number(second_number));
     document.getElementById("first_function_answer").innerHTML = ("Adding both numbers you get: " + answer);
-}
-
-
+  }
+  
+  
