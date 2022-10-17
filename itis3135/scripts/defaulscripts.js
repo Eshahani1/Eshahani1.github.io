22120aaf1168773b73db9919e9e3f3b55c86eb2e
Eshahani1 <112349703+Eshahani1@users.noreply.github.com>
2022-10-17 13:40:30 -0400

    adding in changes

--- itis3135/scripts/defaulscripts.js
+++ itis3135/scripts/defaulscripts.js
@@ -1,30 +1,29 @@
 function scriptTest() {
-    alert("Hey my script is running");
-  }
-  
-  function welcoming_user() {
-    let first = document.getElementById('first_name').value;
-    let mood = document.getElementById('mood').value;
-    document.getElementById("answers").innerHTML = ("The ES welcomes you, " + first + "." + " we're glad you are doing " + mood + "!");
-  }
-  
-  function display_date() {
-    let today = new Date().toLocaleDateString();
-    let today2 = new Date().toLocaleTimeString();
-    document.getElementById("current_date").innerHTML = ("The current Date is: " + today + " The local time is: " + today2);
-  }
-  
-  function simpleAddition() {
-    let first_number = document.getElementById("first_number").value;
-    let second_number = document.getElementById("second_number").value;
-    let answer = (Number(first_number) + Number(second_number));
-    document.getElementById("first_function_answer").innerHTML = ("Adding both numbers you get: " + answer);
-  }
-  function simpleSubtraction() {
-    let first_number = document.getElementById("first_number_sub").value;
-    let second_number = document.getElementById("second_number_sub").value;
-    let answer = first_number - second_number;
-    document.getElementById("second_function_answer").innerHTML = ("Subtracting both numbers you get: " + answer);
-  }
-  
-  
+  alert("Hey my script is running");
+}
+
+function welcoming_user() {
+  let first = document.getElementById('first_name').value;
+  let mood = document.getElementById('mood').value;
+  document.getElementById("answers").innerHTML = ("The ES welcomes you, " + first + "." + " we're glad you are doing " + mood + "!");
+}
+
+function display_date() {
+  let today = new Date().toLocaleDateString();
+  let today2 = new Date().toLocaleTimeString();
+  document.getElementById("current_date").innerHTML = ("The current Date is: " + today + " The local time is: " + today2);
+}
+
+function simpleAddition() {
+  let first_number = document.getElementById("first_number").value;
+  let second_number = document.getElementById("second_number").value;
+  let answer = (Number(first_number) + Number(second_number));
+  document.getElementById("first_function_answer").innerHTML = ("Adding both numbers you get: " + answer);
+}
+function simpleSubtraction() {
+  let first_number = document.getElementById("first_number_sub").value;
+  let second_number = document.getElementById("second_number_sub").value;
+  let answer = first_number - second_number;
+  document.getElementById("second_function_answer").innerHTML = ("Subtracting both numbers you get: " + answer);
+}
+
