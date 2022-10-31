210da550a3c9fd19db4a3b8f1a040c7056f9c0e2
Eshahani <89101607+Eshahani@users.noreply.github.com>
2022-10-31 00:54:01 -0400

    changing function names

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -2,13 +2,13 @@ function scriptTest() {
   alert("Hey my script is running");
 }
 
-function welcoming_user() {
+function promptUser() {
   let first = document.getElementById('first_name').value;
   let mood = document.getElementById('mood').value;
   document.getElementById("answers").innerHTML = ("The ES welcomes you, " + first + "." + " we're glad you are doing " + mood + "!");
 }
 
-function display_date() {
+function getTodaysDate() {
   let today = new Date().toLocaleDateString();
   let today2 = new Date().toLocaleTimeString();
   document.getElementById("current_date").innerHTML = ("The current Date is: " + today + " The local time is: " + today2);
