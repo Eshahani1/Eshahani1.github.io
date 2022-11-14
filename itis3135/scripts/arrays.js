f65287bb52c8bf0200e98f6d0364e270039f1629
Eshahani <89101607+Eshahani@users.noreply.github.com>
2022-11-13 22:00:51 -0500

    adding arrays

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,23 @@
+const person = [Eyaan, Jared, Scump];
+const salaries = [10000, 20000, 30000];
+let sum = 0;
+
+function displayResults(){
+
+}
+
+
+
+function displaySalary(){
+    for(let i = 0; i<salaries.length; i++){
+        sum += salaries[i];
+        document.getElementById("results").innerHTML = ("The average of the salaries is: " + sum);
+    }
+}
+
+function addSalary(){
+
+}
+
+
+
