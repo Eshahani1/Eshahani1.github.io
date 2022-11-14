16a72158df98679e219270e60ba962c6fcbec3bc
Eshahani <89101607+Eshahani@users.noreply.github.com>
2022-11-14 01:16:34 -0500

    Fixing

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -3,22 +3,19 @@ const salaries = [10000, 20000, 30000];
 let sum = 0;
 
 function displayResults(){
-    for(let i = 0; i < salaries.length; i++){
-        for(let j =0; i < person.length; j++){
-            documet.getElementById("people_in").innerHTML=("The people in the system are: " + j)
-            documet.getElementById("salary_in").innerHTML=("The people in the system are: " + i)
-        }
-    }
-}
 
-
-
-function displaySalary(){
     for(let i = 0; i<salaries.length; i++){
         sum += salaries[i];
         sum = sum/salaries.length
         document.getElementById("results").innerHTML = ("The average of the salaries is: " + sum);
     }
+
+}
+
+
+
+function displaySalary(){
+
 }
 
 function addSalary(){
